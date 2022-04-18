67c5b773399b58bd17f509947e8fbf86330a2c18
Harald Fielker <harald.fielker@gmail.com>
2022-04-18 21:50:02 +0200

    extracted params + silence detection

--- audacity.py
+++ audacity.py
@@ -30,6 +30,7 @@ class Audacity():
     def clear(self):
         self._do_command("SelectAll")
         self._do_command("RemoveTracks")
+        self.reference_clips_length = -1
 
     def play_record(self, reference_sample=""):
         """Import track and record to new track.
@@ -40,7 +41,6 @@ class Audacity():
             samples_dir =  os.path.join(script_dir, 'samples')
             reference_sample = os.path.join(samples_dir, 'silence-10sec.wav')
 
-
         self._do_command(f"Import2: Filename={reference_sample}")
         self._do_command('Select: Track=0')
         self._do_command('SelTrackStartToEnd')
@@ -50,17 +50,43 @@ class Audacity():
         clipsinfo = clipsinfo[:clipsinfo.rfind('BatchCommand finished: OK')]
 
         clips = json.loads(clipsinfo)
-        duration = clips[0]['end'] - clips[0]['start']
+        self.reference_clips_length = clips[0]['end'] - clips[0]['start']
 
         # Now we can start recording.
         self._do_command('Record2ndChoice')
         print('Sleeping until recording is complete...')
-        time.sleep(duration + 0.1)
+        time.sleep(self.reference_clips_length + 0.1)
 
-    def truncate_silence(self):
+    def truncate_silence(self) -> bool:
+        # we must have a reference clip loaded
+        if self.reference_clips_length < 0:
+            return False
+
+        # TODO: Fix this!
+        compress=50
+        independent=0
+        minimum=0.001
+        threshold=-20
+        truncate=0.001
+
         self._do_command('Select: Track=1 mode=Set')
         self._do_command('SelectAll')
-        self._do_command('TruncateSilence: Action="Truncate Detected Silence" Compress="50" Independent="0" Minimum="0.001" Threshold="-20" Truncate="0.001"')
+        self._do_command(f"TruncateSilence: Action=\"Truncate Detected Silence\" Compress=\"{compress}\" Independent=\"{independent}\" Minimum=\"{minimum}\" Threshold=\"{threshold}\" Truncate=\"{truncate}\"")
+
+        # check after the truncate if we have a working track
+        clipsinfo = self._do_command('GetInfo: Type=Clips')
+        clipsinfo = clipsinfo[:clipsinfo.rfind('BatchCommand finished: OK')]
+
+        clips = json.loads(clipsinfo)
+        truncate_clip_length = clips[0]['end'] - clips[0]['start']
+
+        # minimum of 100ms
+        if truncate_clip_length < 0.1:
+            return False
+
+        if truncate_clip_length < self.reference_clips_length:
+            return True
+        return False
 
     def export(self, filename):
         """Export the new track, and deleted both tracks."""
